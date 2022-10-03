4701ac12f6dbc1e3dc3ef66e648166c838c6f075
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-03 14:16:59 -0300

    ..

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("hi")
+window.addEventListener('load', () =>{
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-form");
+    const list_el = document.querySelector("#tasks");
+
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault(); /*para página não fazer o reload*/ 
+
+        const task = input.value;
+
+        if(!task){
+            alert("Please fill out the task!");
+            /*return;*/
+        }else{
+            console.log("Success!!")
+        }
+
+        /*const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
+
+        task_el.appendChild(task_content_el);
+
+        list_el.appendChild(task_el);*/
+
+
+   })
+
+})
