7e19dd7d83f473faf9674db258842d8bba4ecb08
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-04 14:02:20 -0300

    ..

--- to_do.js
+++ to_do.js
@@ -1,14 +1,16 @@
 window.addEventListener('load', () =>{
     const form = document.querySelector("#new-task-form");
-    const input = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
 
     form.addEventListener('submit', (e) =>{
         e.preventDefault(); /*para página não fazer o reload*/ 
 
-        const task = input.value;
 
-        if(!task){
+
+        const tasks = input.value;
+
+        if( !tasks ){
             alert("Please fill out the task!");
             /*return;*/
         }else{
