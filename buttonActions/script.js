838d274d713b93bcb2abaf7bd99d38900e3e6820
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-10 15:54:30 -0300

    ...

--- buttonActions/script.js
+++ buttonActions/script.js
@@ -1,4 +1,5 @@
-window.onload = function() {
+// window.onload = function() {
+
     // document.querySelector('#green').onclick = function() {
     //     document.querySelector('#hello').style.color = 'green';
     // };
@@ -9,10 +10,35 @@ window.onload = function() {
     //     document.querySelector('#hello').style.color = 'red';
     // };
 
-    document.querySelectorAll('.color-change').forEach(function(button) {
-        button.onclick = function() {
-            document.querySelector('#hello').style.color = button.dataset.color;
-        };
+
+
+    // document.querySelectorAll('.color-change').forEach(function(button) {
+    //     button.onclick = function() {
+    //         document.querySelector('#hello').style.color = button.dataset.color;
+    //     };
+    // });
+
+
+
+    const questions = [
+        {
+            question: "What is 10 + 10?",
+            options: ["8", "25", "30", "20"],
+            answer: "20"
+        },
+        {
+            question: "What is Athena's favorite animal?",
+            options:["jellyfish", "penguins", "otters"],
+            answer:"otters"
+        }
+    ];
+
+    document.addEventListener("DOMContentLoaded", () => {
+        load_question();
     });
 
-};
+    function load_question() {
+        document.querySelectorAll("#questions").innerHTML.questions[questions_number].question;
+    }
+
+// };
