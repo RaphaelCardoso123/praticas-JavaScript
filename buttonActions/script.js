c6165689232ba34116c47132b603766f26c18348
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-10 11:34:16 -0300

    querySelector/querySelectorAll/forEach/data-

--- buttonActions/script.js
+++ buttonActions/script.js
@@ -1,15 +1,18 @@
 window.onload = function() {
+    // document.querySelector('#green').onclick = function() {
+    //     document.querySelector('#hello').style.color = 'green';
+    // };
+    // document.querySelector('#blue').onclick = function() {
+    //     document.querySelector("#hello").style.color = 'blue';
+    // };
+    // document.querySelector('#red').onclick = function() {
+    //     document.querySelector('#hello').style.color = 'red';
+    // };
 
-    document.querySelector('#green').onclick = function() {
-        document.querySelector('#hello').style.color = 'green';
-    };
-
-    document.querySelector('#blue').onclick = function() {
-        document.querySelector("#hello").style.color = 'blue';
-    };
-
-    document.querySelector('#red').onclick = function() {
-        document.querySelector('#hello').style.color = 'red';
-    };
+    document.querySelectorAll('.color-change').forEach(function(button) {
+        button.onclick = function() {
+            document.querySelector('#hello').style.color = button.dataset.color;
+        };
+    });
 
 };
