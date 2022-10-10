553d3008f91a0e590da98aad2eac42524e49334e
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-10 11:10:17 -0300

    querySelector

---
+++ buttonActions/script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+
+    document.querySelector("#green").onclick = function() {
+        document.querySelector("#hello").style.color = 'green';
+    };
+
+    document.querySelector("#blue").onclick = function() {
+        document.querySelector("#hello").style.color = 'blue';
+    };
+
+    document.querySelector("#red").onclick = function() {
+        document.querySelector("#hello").style.color = 'red';
+    };
+
+};
