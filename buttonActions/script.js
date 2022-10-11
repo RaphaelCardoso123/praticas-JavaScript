8ef0530e38d9b94d1545894c429bbdd63c0380b8
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-11 10:07:43 -0300

    ...

--- buttonActions/script.js
+++ buttonActions/script.js
@@ -22,23 +22,39 @@
 
     const questions = [
         {
-            question: "What is 10 + 10?",
-            options: ["8", "25", "30", "20"],
-            answer: "20"
-        },
-        {
             question: "What is Athena's favorite animal?",
             options:["jellyfish", "penguins", "otters"],
             answer:"otters"
+        },
+        {
+            question: "What is 10 + 10?",
+            options: ["8", "25", "30", "20"],
+            answer: "20"
         }
+        
     ];
+
+    let question_number = 0;
+    let correct = 0;
 
     document.addEventListener("DOMContentLoaded", () => {
         load_question();
     });
 
     function load_question() {
-        document.querySelectorAll("#questions").innerHTML.questions[questions_number].question;
-    }
+        document.querySelector("#questions").innerHTML = questions[question_number].question;
+        const options = document.querySelector ('#options');
+        options.innerHTML = '';
+        
+        for (const option of questions[question_number].options) {
+            options.innerHTML += `<button class="option">${option}</button>`;
+        }
+
+        document.querySelectorAll('.option').forEach(options => {
+            options.onclick = () => {
+                alert(options.textContent);
+            }
+        });
+    };
 
 // };
